3e637545107e61fe61f5ac06eada2e6d21aa2705
Rajat Rajput <rajputrajat@gmail.com>
2021-09-17 15:18:51 +0530

    file is read

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use log::info;
+use serde::Deserialize;
 use std::{env, process::exit, time::Instant};
 use svn_cmd::{Credentials, SvnCmd, SvnList};
 
@@ -21,17 +22,51 @@ fn get_svn_path_from_cli_args() -> String {
     }
 }
 
-async fn get_svn_list(path: &str) -> Result<SvnList> {
-    let svn_cmd = SvnCmd::new(
-        Credentials {
-            username: "svc-p-blsrobo".to_owned(),
-            password: "Comewel@12345".to_owned(),
-        },
-        None,
-    )?;
-    Ok(svn_cmd.list(path, true).await?)
+struct SvnCommand {
+    cmd: SvnCmd,
 }
 
-fn parse_cds_config_and_check_lines(cds_config_path: &str) -> Result<()> {
-    Ok(())
+#[derive(Deserialize)]
+struct GameConfiguration {
+    line_options: LineOptions,
+}
+
+#[derive(Deserialize)]
+struct LineOptions {
+    line_option: LineOption,
+}
+
+#[derive(Deserialize)]
+struct LineOption {
+    line: Vec<Line>,
+}
+
+#[derive(Deserialize)]
+struct Line {}
+
+impl SvnCommand {
+    fn new() -> Result<Self> {
+        Ok(Self {
+            cmd: SvnCmd::new(
+                Credentials {
+                    username: "svc-p-blsrobo".to_owned(),
+                    password: "Comewel@12345".to_owned(),
+                },
+                None,
+            )?,
+        })
+    }
+
+    async fn get_svn_list(&self, path: &str) -> Result<SvnList> {
+        Ok(self.cmd.list(path, true).await?)
+    }
+
+    async fn parse_cds_config_and_check_lineoptions_count(
+        &self,
+        cds_config_path: &str,
+    ) -> Result<usize> {
+        let xml_text = self.cmd.cat(cds_config_path).await?;
+        let config: GameConfiguration = serde_xml_rs::from_str(&xml_text)?;
+        Ok(config.line_options.line_option.line.len())
+    }
 }
