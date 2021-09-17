e7d2e47e6bf59b8406b02257610df5e693c4778a
Rajat Rajput <rajputrajat@gmail.com>
2021-09-17 15:47:02 +0530

    wip. async concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use async_std::task;
 use log::info;
 use serde::Deserialize;
 use std::{env, process::exit, time::Instant};
@@ -22,6 +23,7 @@ fn get_svn_path_from_cli_args() -> String {
     }
 }
 
+#[derive(Clone)]
 struct SvnCommand {
     cmd: SvnCmd,
 }
@@ -69,4 +71,36 @@ impl SvnCommand {
         let config: GameConfiguration = serde_xml_rs::from_str(&xml_text)?;
         Ok(config.line_options.line_option.line.len())
     }
+}
+
+const PATH_FILTERS: &[&str] = &["tags", "_signed", "cds_configuration.xml"];
+
+fn find_cfg_file_paths(path: &str, svn_list: SvnList) -> Vec<String> {
+    svn_list
+        .iter()
+        .filter_map(|e| {
+            let full_path = format!("{}/{}", path, e.name);
+            if PATH_FILTERS.iter().all(|f| full_path.contains(f)) {
+                Some(full_path)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+async fn process(path: &str) -> Result<()> {
+    let cmd = SvnCommand::new()?;
+    let list = cmd.get_svn_list(path).await?;
+    let cfg_files = find_cfg_file_paths(path, list);
+    let mut tasks = Vec::new();
+    for cfg_file in cfg_files.into_iter() {
+        let cmd = cmd.clone();
+        tasks.push(task::spawn(async move {
+            cmd.parse_cds_config_and_check_lineoptions_count(&cfg_file)
+                .await
+                .unwrap();
+        }));
+    }
+    Ok(())
 }
