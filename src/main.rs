e0a97ad3d3cc9ce61b38bef43e59ba06c5bebe0d
Rajat Rajput <rajputrajat@gmail.com>
2021-09-17 14:47:44 +0530

    wip. adding parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use log::info;
+use std::{env, process::exit, time::Instant};
+use svn_cmd::{Credentials, SvnCmd, SvnList};
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+    let start_instant = Instant::now();
+    let root_path = get_svn_path_from_cli_args();
+
+    Ok(())
+}
+
+fn get_svn_path_from_cli_args() -> String {
+    if let Some(path) = env::args().nth(1) {
+        path
+    } else {
+        eprintln!("provide svn path as cli argument.");
+        exit(1);
+    }
+}
+
+async fn get_svn_list(path: &str) -> Result<SvnList> {
+    let svn_cmd = SvnCmd::new(
+        Credentials {
+            username: "svc-p-blsrobo".to_owned(),
+            password: "Comewel@12345".to_owned(),
+        },
+        None,
+    )?;
+    Ok(svn_cmd.list(path, true).await?)
+}
+
+fn parse_cds_config_and_check_lines(cds_config_path: &str) -> Result<()> {
+    Ok(())
 }
